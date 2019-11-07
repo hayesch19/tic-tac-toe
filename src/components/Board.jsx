c7d5d791bcddd9467a9ff8393c5633521f29b76d
hayesch19 <chrishayespro@gmail.com>
2019-11-07 11:11:26 -0500

    Board And Square Components Added

---
+++ src/components/Board.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Board = () => {
+  return (
+    <div>
+      <h3>Game Board</h3>
+    </div>
+  )
+}
+
+export default Board
---
+++ src/components/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = () => {
+  return (
+    <div>
+      <h3>Game Square</h3>
+    </div>
+  )
+}
+
+export default Square
