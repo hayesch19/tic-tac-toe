9b750970e072bd6af7cb4d1875e037a7d9398186
hayesch19 <chrishayespro@gmail.com>
2019-11-08 09:10:41 -0500

    Header Added And CSS Changes

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,21 +7,26 @@ const Board = () => {
   const [move, setMove] = useState(Math.random() > 0.5 ? 5 : 3)
 
   return (
-    <div className="game-board">
-      <div className="row">
-        <Square number={0} />
-        <Square number={1} />
-        <Square number={2} />
+    <div className="game-area">
+      <div className="header">
+        <h1>Tic Tac Toe</h1>
       </div>
-      <div className="row">
-        <Square number={3} />
-        <Square number={4} />
-        <Square number={5} />
-      </div>
-      <div className="row">
-        <Square number={6} />
-        <Square number={7} />
-        <Square number={8} />
+      <div className="game-board">
+        <div className="row">
+          <Square number={0} />
+          <Square number={1} />
+          <Square number={2} />
+        </div>
+        <div className="row">
+          <Square number={3} />
+          <Square number={4} />
+          <Square number={5} />
+        </div>
+        <div className="row">
+          <Square number={6} />
+          <Square number={7} />
+          <Square number={8} />
+        </div>
       </div>
     </div>
   )
