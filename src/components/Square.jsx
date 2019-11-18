21f94abc3d09aaca6fcb5f00bfba28377afad403
hayesch19 <chrishayespro@gmail.com>
2019-11-18 10:17:44 -0500

    X's And O's Imported

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { ReactComponent as X } from '../images/X.svg'
+import { ReactComponent as O } from '../images/O.svg'
 
-export class Square extends Component {
-  render() {
-    return (
-      <>
-        <button className="square" onClick={this.props.handleClick}></button>
-      </>
-    )
-  }
+function Square(props) {
+  const number = props.number
+  const [move, setMove] = props.move
+  const [board, setBoard] = props.board
+  const [winner, setWinner] = props.winner
+
+  return (
+    <>
+      <button></button>
+    </>
+  )
 }
 
 export default Square
