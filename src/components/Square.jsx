fb2bb47f1925ef6906d1c1d6200bd2578c306faa
hayesch19 <chrishayespro@gmail.com>
2019-11-18 13:59:19 -0500

    Square Component Logic Added

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -8,10 +8,47 @@ function Square(props) {
   const [board, setBoard] = props.board
   const [winner, setWinner] = props.winner
 
+  const Image = props => {
+    if (props.boardN === 5) {
+      return (
+        <X
+          fill={winner.indexOf(number) !== -1 ? 'red' : ''}
+          width="100%"
+          height="100%"
+        />
+      )
+    } else if (props.boardN === 3) {
+      return (
+        <O
+          fill={winner.indexOf(number) !== -1 ? 'red' : ''}
+          width="100%"
+          height="100%"
+        />
+      )
+    } else {
+      return <img />
+    }
+  }
+
   return (
-    <>
-      <button></button>
-    </>
+    <div
+      className="square"
+      onClick={() => {
+        if (board[number] === 0 && winner.length === 0) {
+          setBoard(
+            (() => {
+              let newBoard = [...board]
+              let newMove = move === 3 ? 5 : 3
+              newBoard[number] = newMove
+              setMove(newMove)
+              return newBoard
+            })()
+          )
+        }
+      }}
+    >
+      <Image boardN={board[number]} />
+    </div>
   )
 }
 
